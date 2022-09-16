0b482880222bb3f74e8eec40fed31a9a818fbc22
maul49zer@gmail.com <maul49zer@gmail.com>
2022-09-16 12:06:32 -0600

    made some final changes

--- app.js
+++ app.js
@@ -50,31 +50,34 @@ function whichQuadrant(x, y) {
     } else if (x > 0 && y < 0) {
         console.log(`${x}, ${y} is in Quadrant 4!`)
     } else if (x == 0 && y != 0) {
-        console.log(`${x}, ${y} is on the X axis!`)
-    } else if (x != 0 && y == 0) {
         console.log(`${x}, ${y} is on the Y axis!`)
+    } else if (x != 0 && y == 0) {
+        console.log(`${x}, ${y} is on the X axis!`)
     } else if (x == 0 && y == 0) {
         console.log(`${x}, ${y} is on the Origin!`)
     }
 }
 whichQuadrant(0, 0);
-//console.log(`${x}, ${y}`)
 
 // Exercise 4 Section
 console.log("EXERCISE 4:\n==========\n");
 
 function triangleCheck(a, b, c) {
-    if (a + b <= c || b + c <= a) {
-        console.log("Invalid Triangle");
-    } else if (a == b && b != c || a == c && c != b) {
-        console.log("Isosceles Triangle");
+    if (a + b <= c || b + c <= a || a + c <= b) {
+        console.log(`${a}, ${b}, ${c} is an Invalid Triangle`);
+    } else if (a == b && b != c || a == c && c != b || b == c && b != a) {       
+        console.log(`${a}, ${b}, ${c} is an Isosceles Triangle`);
     } else if (a == b && a == c) {
-        console.log("Equilateral Triangle");
+        console.log(`${a}, ${b}, ${c} is an Equilateral Triangle`);        
     } else if (a != b || a != c || b != c) {
-        console.log("Scalene Triangle");
+        console.log(`${a}, ${b}, ${c} is a Scalene Triangle`);        
     }
 }
-triangleCheck(1, 2, 8)
+triangleCheck(1, 2, 2)
+triangleCheck(5, 5, 5)
+triangleCheck(3, 4, 5)
+triangleCheck(1, 8, 2)
+
 
 // Exercise 5 Section
 console.log("EXERCISE 5:\n==========\n");
@@ -82,16 +85,25 @@ function dataPlanStatus(planLimit, day, usage) {
     let daysRemaining = 30 - day;
     let averageDailyUse = planLimit / 30;  
     let exceededUse = usage / day;
-    let overallExceededUse = (exceededUse * 30) - planLimit;
+    let overallExceededUse = (exceededUse.toFixed(2) * 30) - planLimit;
     let usageGoal = (planLimit - usage) / daysRemaining;
     if (exceededUse - averageDailyUse > 0) {
         console.log(`${day} days used, ${daysRemaining} days remaining \n
-        Average daily use: ${averageDailyUse}/Day \n
-        You are EXCEEDING your average daily use ${exceededUse} GB/day, \n
-        continuing this high usage, you'll exceed your data plan by ${overallExceededUse} GB. \n
-        To stay below your data plan, use no more than ${usageGoal} GB/day.`); 
-    } else {
-        console.log("Good Job! At this rate you won't exceed your monthly usage.")
+        Average daily use: ${averageDailyUse.toFixed(3)}/Day \n
+        You are EXCEEDING your average daily use ${exceededUse.toFixed(2)} GB/day, \n
+        continuing this high usage, you'll exceed your data plan by ${overallExceededUse.toFixed(1)} GB. \n
+        To stay below your data plan, use no more than ${usageGoal.toFixed(2)} GB/day.`); 
+
+    } else if (exceededUse - averageDailyUse < 0) {
+        console.log(`${day} days used, ${daysRemaining} days remaining \n
+        Average daily use: ${averageDailyUse.toFixed(3)}/Day \n
+        You are UNDER your average daily use ${exceededUse.toFixed(2)} GB/day, \n
+        continuing this usage, you'll be under your data plan by ${Math.abs(overallExceededUse.toFixed(1))} GB. `); 
+    }
+    else {
+        console.log(`Your average daily use is ${averageDailyUse.toFixed(2)}, at this rate you will be at your \n
+        monthly usage by the end of the month.`);
     }
 }
-dataPlanStatus(95, 12, 38)
+dataPlanStatus(100, 15, 56)
+
